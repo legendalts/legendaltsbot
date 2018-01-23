07222dc3e8ae79741f653b3aa5d953ae6a64e9cb
legendalts <35744444+legendalts@users.noreply.github.com>
2018-01-23 22:48:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ client.on('message', msg => {
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
+client.on('message', function(message) {
+    if (message.content == "!clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        }
+    }
+
+});
+
 client.login(process.env.TOKEN);
