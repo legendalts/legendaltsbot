3b3faf5a3f0da24098757cd1b8d0eab30591030b
legendalts <35744444+legendalts@users.noreply.github.com>
2018-01-24 12:56:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 var anti_spam = require("discord-anti-spam");
-anti_spam(client, {
+antispam(client, {
   warnBuffer: 5, //Maximum amount of messages allowed to send in the interval time before getting warned. 
   maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned. 
   interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned. 
   warningMessage: "stop spamming or you will be banned!!!", // Warning message send to the user indicating they are going to fast. 
   banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it. 
-  maxDuplicatesWarning = 7; // Maximum amount of duplicate messages a user can send in a timespan before getting warned 
-  maxDuplicatesBan = 10; // Maximum amount of duplicate messages a user can send in a timespan before getting banned 
+  maxDuplicatesWarning = 7, // Maximum amount of duplicate messages a user can send in a timespan before getting warned 
+  maxDuplicatesBan = 10 // Maximum amount of duplicate messages a user can send in a timespan before getting banned 
 });
 
 
