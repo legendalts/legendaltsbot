ff82087c71026eb5e7ae23a74f2503c7ccc56d4d
legendalts <35744444+legendalts@users.noreply.github.com>
2018-01-24 23:35:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Discord.js bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
-let lastGetaltCommandDate, lastGetaltCommandUser;
+
+// First, this must be at the top level of your code, **NOT** in any event!
+const talkedRecently = new Set();
 
 fs = require('fs')
 var data;
@@ -51,11 +53,21 @@ client.on('message', msg => {
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'getalt') {
-	   msg.channel.send("Check your PM " + msg.author);
-       msg.author.send(':arrow_down: :regional_indicator_a: :regional_indicator_l: :regional_indicator_t: :arrow_down: \n' + getRandomLine() + '\n :regional_indicator_e: :regional_indicator_n: :regional_indicator_j: :regional_indicator_o: :regional_indicator_y: \n :heart_decoration: :heart: :heart_decoration: :heart: :heart_decoration:');
-      //msg.author.send(getRandomLine());
-      //msg.author.send(':regional_indicator_e: :regional_indicator_n: :regional_indicator_j: :regional_indicator_o: :regional_indicator_y:');
-      //msg.author.send(':heart_decoration: :heart: :heart_decoration: :heart: :heart_decoration:');
+		
+			if (talkedRecently.has(msg.author.id)) {
+			msg.channel.send("Check your PM " + msg.author);
+			msg.author.send(':arrow_down: :regional_indicator_a: :regional_indicator_l: :regional_indicator_t: :arrow_down: \n' + getRandomLine() + '\n :regional_indicator_e: :regional_indicator_n: :regional_indicator_j: :regional_indicator_o: :regional_indicator_y: \n :heart_decoration: :heart: :heart_decoration: :heart: :heart_decoration:');
+
+			// Adds the user to the set so that they can't talk for 2.5 seconds
+			talkedRecently.add(msg.author.id);
+			setTimeout(() => {
+			  // Removes the user from the set after 2.5 seconds
+			  talkedRecently.delete(msg.author.id);
+			}, 60000);
+		} else {
+			//nothing yet
+		}		
+	
     }
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
