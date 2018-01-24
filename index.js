fa960af9f9643bdd9237e13c671be6f3ba501b7c
legendalts <35744444+legendalts@users.noreply.github.com>
2018-01-24 10:51:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
+fs = require('fs')
+var data;
+fs.readFile('alts.txt', 'utf8', function (err,rawData) {
+  if (err) {
+    return console.log(err);
+  }
+  data = rawData.split('\n');
+});
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function getRandomLine(){
+  return data[randomInt(0,data.length)];
+}
+
 client.on('ready', () => {
-    client.user.setActivity('0 alts', {type: 'WATCHING'});
+    client.user.setActivity('0 alts', {type: 'PLAYING'});
 });
 
 client.on('message', msg => {
@@ -11,6 +29,14 @@ client.on('message', msg => {
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'guide') return msg.channel.send('Type !getalt to get your alt.');
+    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+});
+
+client.on('message', msg => {
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'getalt') return msg.channel.send(getRandomLine());
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
